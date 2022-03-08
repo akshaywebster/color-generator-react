b404ded68fee69da4bd8b1cf91621ef4907ea941
Akshay Chaudhary <akshaych49@gmail.com>
2022-03-08 14:24:45 +0530

    added copy to clipboard functionality

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,13 +5,27 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bgcolor = rgb.join(',')
   const hexValue = `#${hexColor}`
 
+  const handleClick = () => {
+    setAlert(true)
+    navigator.clipboard.writeText(hexValue)
+  }
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+  }, [alert])
+
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bgcolor})` }}
+      onClick={handleClick}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   )
 }
